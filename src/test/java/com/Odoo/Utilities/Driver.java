99d6d4cc2c6626a860cb06189ed55993682d408f
AkobirTuraev <AkobirfTuraev@gmail.com>
2019-12-23 13:43:45 -0500

    new Information

---
+++ src/test/java/com/Odoo/Utilities/Driver.java
@@ -0,0 +1,17 @@
+package com.Odoo.Utilities;
+
+import org.openqa.selenium.WebDriver;
+
+public class Driver {
+    WebDriver driver;
+
+    public void Type(String browser){
+
+        switch ("chrome"){
+
+        }
+
+    }
+
+
+}
